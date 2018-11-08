a9900fff54ccdd39fe213ed23ada9fed65a8700a
Jose Barajas <barajas.jose452yahoo.com>
2018-11-07 20:39:37 -0800

    dayTwoTakehome done

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,9 @@
-// variable string primitive  var name = "Jose";
-//variable string object  var nameTwo = new String("Jose");
-// variable number  var fignumber = 56;
-// string "68";
-// true or false  var boolean = true ;
-//var mystery ; console.log(typeof(mystery)); undefined is a lack of definition
-//console.log(null); null is the lack of a value
-//console.log(typeof(null)); 6th data type objects
-//console.log(typeof(Symbol())); new 7th type symbols
+const firstName = prompt('What is your first name?');
+const lastName = prompt('Whats your last name?')
+const birthday = new Date(prompt('What is your birthday'));
 
-// new reserved keyword
-
-// const name ="jose";
-
-// console.log(`Lorem ipsum, dolor sit amet consectetur ${name} adipisicing elit. Optio, adipisci praesentium quae culpa labore aut, nesciunt exercitationem sint est nobis magni earum, vero laboriosam voluptatum atque consequatur iusto commodi quod!`);
-
-// const date = new Date();
-
-// const num = new Number(34);
-// const num3 =34;
-
-// const array = [1,2,3,3,4];
-// const array2 = new Array(1,2,3,3,4);
-// const arrayMixed = ["Jose",23,true];
-
-// const obj= {
-//     name: "Jose",
-//     age: 24
-// };
-// console.log(obj);
-
-
-// alert("Danger Will Robinson!!!");
-
-//const response = confirm('Are You Sure?');
-
-//console.log(response);
-
-const favNum = prompt('What is your favorite number?');
-
-const realNum = parseInt(favNum);
-// parseInt tells the code its a number
-
-const num = "66";
-
-
-console.log();
-
-
+console.log(firstName);
+alert(lastName);
+console.log(birthday);
+confirm(`Is your birthday correct: ${birthday}`);
+alert(`your birthday is: ${birthday}`);
