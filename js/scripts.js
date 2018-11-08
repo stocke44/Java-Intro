dd37e4393505a6164c6d739d7d35fc77250a41e1
Jose Barajas <barajas.jose452yahoo.com>
2018-11-07 21:01:46 -0800

    dayThree done

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
+//console.log('42' == 42);  loose equals (assumes you where comparing number)
+//console.log('5' === 5);  strict equals (compares a string with a number)
+
+//console.log("42" != 42); loose not equals 
+//console.log('42' !== 42); strict not equals
+
+// const obj1 ={
+//     name: 'jose'
+// };
+
+// const obj2 ={
+//     name: 'jose'
+// };
+
+
+// const arr1 = ['cat'];
+// const arr2 = ['cat'];
+//arrays are objects
+//console.log(obj1.toString() == obj2.toString());  objects are never equal because there in two dir]fferent places in memory
+
+//const age = prompt('How old are you?');
+
+//console.log(age>=21);
+
+// const num = '42';
+// console.log(num);
+// console.log(parseInt(num)); same as console.log(+num); this one is better to use for numbers with a decimal
 
